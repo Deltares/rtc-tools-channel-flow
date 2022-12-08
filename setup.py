fe654b9655fe4f181343e5e13da8f2ab9d7b28f1
Ailbhe <ailbhe.mitchell@deltares.nl>
2022-12-08 16:41:49 +0100

    Setup: RTC-Tools requirement
    enforce minimim RTC-Tools such that existing version is
    not uninstalled in favour of 2.4.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     license="LGPL",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["rtc-tools",
+    install_requires=["rtc-tools>=2.0.0",
                       "pymoca >= 0.4.2"],
     include_package_data=True,
     cmdclass=versioneer.get_cmdclass(),
